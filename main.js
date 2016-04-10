08b17472ad0664a5d69d586284904c205434cef2
Kate Shestakova <kate.shestakova@me.com>
2016-04-10 13:00:21 +0300

    set marker for EPAM offices

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 53.9056591, lng: 27.5598183},
+    zoom: 10
   });
+}
+
+var officesSelect = document.getElementById("offices");
+var officesOptions = officesSelect.getElementsByTagName("option");
+var offices = {};
+
+for(var i = 0; i < officesOptions.length; i++) {
+  offices[officesOptions[i].value] = {lat: +officesOptions[i].getAttribute("data-lat"), lng: +officesOptions[i].getAttribute("data-lng")};
+}
+console.log(offices);
+
+
+
+officesSelect.addEventListener("change", chooseOffice);
+function chooseOffice(event) {
+  if(officesSelect.value == "") return;
+  map.setCenter(offices[officesSelect.value]);
+  map.setZoom(17);
+  new google.maps.Marker({position: offices[officesSelect.value], map: map}); 
 }
