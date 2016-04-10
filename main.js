2b0b327b355bad261557eb418652799ab240a89c
Kate Shestakova <kate.shestakova@me.com>
2016-04-10 17:00:48 +0300

    click on the map to set "home"

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
 
 var input = document.getElementById('home');
 var map, autocomplete, homeMarker, officeMarker, homeLocation, officeLocation;
+var x;
 
 //Create map with center in Minsk
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 53.9056591, lng: 27.5598183},
-    zoom: 10
+    zoom: 12
   });
   
   //This is for autocomplete address
   autocomplete = new google.maps.places.Autocomplete(input);
-  
   autocomplete.addListener('place_changed', setHome);
   
   //Create reusable markers
   homeMarker = new google.maps.Marker({position: {lng: 0, lat: 0}, map: map, visible: false, draggable: true});
   
   officeMarker = new google.maps.Marker({position: {lng: 0, lat: 0}, map: map, visible: false});
+  
+  //Fill input with address
+  var geocoder = new google.maps.Geocoder;
+
+  map.addListener("click", function(event) {
+    homeLocation = event.latLng;
+    setMarker(homeMarker, homeLocation);
+    if(officeLocation) fitBounds();
+    
+    geocoder.geocode({'location': homeLocation}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        input.value = results[0].formatted_address;
+      } else {
+        console.warn("Geocode error", status);
+      }
+    });
+  });  
 }
+
+
+
+
 
 //Create object with offices latitude and longitude
 var officesSelect = document.getElementById("offices");
@@ -62,6 +83,7 @@ function setMarker(marker, location) {
   marker.setPosition(location);
 }
 
+//Zoom to fit home and office addresses on the map
 function fitBounds() {
   var bounds = new google.maps.LatLngBounds();
   //extend is for recogtize sw and ne coords
@@ -69,5 +91,6 @@ function fitBounds() {
   bounds.extend(officeLocation);
   map.fitBounds(bounds);
 }
+
 var form = document.getElementsByTagName("form")[0];
 form.addEventListener("submit", function() {event.preventDefault()})
